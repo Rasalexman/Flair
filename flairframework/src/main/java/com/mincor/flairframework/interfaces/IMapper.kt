72095662b48ef858255748de0f32fd64df1d6745
Alexander <sphc@yandex.ru>
2018-08-09 10:41:51 +0300

    1.2.0 change package name

--- flairframework/src/main/java/com/mincor/flairframework/interfaces/IMapper.kt
+++
@@ -1,9 +0,0 @@
-package com.mincor.flairframework.interfaces
-
-/**
- * Helper interface for bind generic instances with name
- */
-interface IMapper<T> {
-    val instanceMap: MutableMap<String, T>
-}
-inline fun <reified T> IMapper<T>.instance(key:String, inst:()->T):T = this.instanceMap.getOrPut(key) { inst() }
--- flairframework/src/main/java/com/mincor/flairframework/interfaces/common/IActionBarProvider.kt
+++
@@ -1,6 +0,0 @@
-package com.mincor.flairframework.interfaces.common
-
-interface IActionBarProvider<out AB, in T> {
-    fun getSupportActionBar():AB?
-    fun setSupportActionBar(toolbar:T?)
-}
