954d1b0caec60659cb656a1e8f4e459c17fb6743
Alexander <sphc@yandex.ru>
2018-08-12 20:10:24 +0300

    exaple with LiveData

---
+++ app/src/main/java/com/mincor/flair/proxies/LiveDataProxy.kt
@@ -0,0 +1,19 @@
+package com.mincor.flair.proxies
+
+import android.arch.lifecycle.MutableLiveData
+import com.mincor.flair.proxies.vo.AccountModel
+import com.rasalexman.flairframework.patterns.proxy.Proxy
+import java.util.*
+
+class LiveDataProxy : Proxy<MutableLiveData<AccountModel>>(MutableLiveData()) {
+
+    override fun onRegister() {
+        changeData()
+    }
+
+    fun changeData() {
+        data?.value = AccountModel(UUID.randomUUID().toString(), "id_${rand(1, 100_000)}")
+    }
+}
+
+fun rand(s: Int, e: Int) = Random().nextInt(e + 1 - s) + s
